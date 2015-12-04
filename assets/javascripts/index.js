b935fd20409313a9f25675b8b2f28f71b9a95aca
Lorefnon <lorefnon@gmail.com>
2015-12-04 22:46:27 +0530

    Fix issues

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{"default":e}}var _tls_check=require("./tls_check"),_tls_check2=_interopRequireDefault(_tls_check);(0,_tls_check2["default"])();
+"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{"default":e}}var _tls_check=require("./tls_check"),_tls_check2=_interopRequireDefault(_tls_check);!function(){(0,_tls_check2["default"])()}();
 
 },{"./tls_check":3}],2:[function(require,module,exports){
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]={domain:"lorefnon.me"};
