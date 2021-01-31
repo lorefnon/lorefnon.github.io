7402d250d6c11a33fa5c65bf151022d9ee2c619c
lorefnon <lorefnon@gmail.com>
2021-02-01 00:47:49 +0530

    Update content

---
+++ wp-content/plugins/metronet-profile-picture/js/mpp-frontend.js
@@ -0,0 +1,12 @@
+(() => {
+  // source/wp-content/plugins/metronet-profile-picture/js/mpp-frontend.js
+  jQuery(document).ready(function($) {
+    $(".mpp-gutenberg-tab").on("click", function(e) {
+      $(".mpp-author-tabs li").removeClass("active");
+      $(this).addClass("active");
+      var $tabs = $(".mpp-tab").removeClass("mpp-tab-active");
+      var new_tab = $(this).data("tab");
+      $("." + new_tab).addClass("mpp-tab-active");
+    });
+  });
+})();
