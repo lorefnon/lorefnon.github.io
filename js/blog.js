16e4fad533ffbd582346cab19df775dac168962f
lorefnon <lorefnon@gmail.com>
2021-03-30 00:09:17 +0530

    Update content

--- js/blog.js
+++ js/blog.js
@@ -1,2 +1,22 @@
 (() => {
+  // source/js/_fold.js
+  document.addEventListener("click", (e) => {
+    debugger;
+    let handle;
+    if (e.target.classList.contains("hlcode-fold-handle")) {
+      hadle = e.target;
+    } else if (e.target.classList.contains("hlcode-fold-text")) {
+      handle = e.target.parentElement;
+    }
+    if (!handle)
+      return;
+    const {foldId} = handle.dataset;
+    if (!foldId)
+      return;
+    handle.remove();
+    const foldedLines = document.querySelectorAll(`.hlcode-line-fold[data-fold-id="${foldId}"]`);
+    for (let i = 0; i < foldedLines.length; i++) {
+      foldedLines[i].classList.remove("hidden");
+    }
+  });
 })();
