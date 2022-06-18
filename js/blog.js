39a801ef95d884da5f6ac16b190f038ed2e4fb10
lorefnon <lorefnon@users.noreply.github.com>
2022-06-18 18:13:29 +0000

    deploy: 7626aebd18710fab373181b3d2b9bb943babdbb7

--- js/blog.js
+++ js/blog.js
@@ -74,7 +74,7 @@
   };
 
   // source/js/_analytics.js
-  window.goatcounter = {no_onload: true};
+  window.goatcounter = window.goatcounter || {no_onload: true};
   htmx.on("htmx:load", (e) => {
     window.goatcounter.count({
       path: location.pathname + location.search + location.hash
